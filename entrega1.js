e278f67c2522422332a2274d0d3e109a44019fda
Matias calderazzi <calderazzi10@gmail.com>
2023-03-23 20:49:23 -0300

    segundo Commit

--- entrega1.js
+++ entrega1.js
@@ -1,67 +1,76 @@
-function datos(dia, mes, hora) {
-    let validar = true;
-    if ((dia) || dia < 1 || dia > 31) {
-        validar = false;
-        alert("El dia es invalido");
-    }
-    if ((mes) || mes < 1 || mes > 12) {
-        validar = false;
-        alert("El mes es invalido");
-    }
-    if ((hora) || hora < 1 || hora > 23) {
-        validar = false;
-        alert("la hora es invalida");
-    }
-    return validar;
-}
-//pedir los datos de la cita
-let nombre = prompt("Ingresa tu nombre");
-let dia = prompt("Ingrese dia [1 -31]");
-let mes = prompt("Ingrese Mes [1-12]");
-let hora = prompt("Ingrese hora [9-18]");
 
+let ArticulosEnVenta= []; // Array vacío para almacenar los artículos en venta
+let ArticuloVendidos = []; // Array vacío para almacenar los artículos vendidos
 
-
-let=direccion=0;
-
-
-    direccion = parseInt(prompt(`
-                ingrese 1 si desea la direccion: Av. La Plata 1700
-                Ingrese 2 si desea la direccion: Varela 550
-                ingrese 3 si desea la direccion: Galicia 2217
-                Ingrese 4 si desea la direccion: Espinosa 4565
-                Ingrese 5 si desea la direccion: Cabildo 1222`));
-
-
-if(direccion==1)
-    alert(`Su turno es el dia ${dia}/${mes} a las ${hora} en Av. La Plata 1700`)
-else if(direccion==2)
-    alert(`Su turno es el dia ${dia}/${mes} a las ${hora} en Varela 550`)
-else if(direccion==3)
-    alert(`Su turno es el dia ${dia}/${mes} a las ${hora} en Galicia 2217`)
-else if(direccion==4)
-    alert(`Su turno es el dia ${dia}/${mes} a las ${hora} en Espinosa 4565`)
-else if(direccion==5)
-    alert(`Su turno es el dia ${dia}/${mes} a las ${hora} en Cabildo 1222`)
-else{
-    alert("su opcion no es valida");
+// Funciones esenciales
+function AgregarItem(item) {
+    ArticulosEnVenta.push(item); // Agrega un artículo a la lista de artículos en venta
+  alert("El artículo se ha agregado correctamente.");
 }
 
-//calcular tiempo de esperar/
-let tiempoTotal = 0;
-let cantidadDeTurnos = 0;
+function sellItem(index) {
+  const item = ArticulosEnVenta.splice(index, 1)[0]; // Elimina el artículo vendido del array de artículos en venta y lo guarda en una variable
+  ArticuloVendidos.push(item); // Agrega el artículo vendido al array de artículos vendidos
+  alert(`El artículo '${item.nombre}' se ha vendido por $${item.precio}.`);
+}
 
-let seguirAgregando = true;
-while (seguirAgregando) {
-  let tiempoDeEspera = parseInt(prompt("Ingrese el tiempo de espera del paciente en minutos:"));
-  tiempoTotal += tiempoDeEspera;
-  cantidadDeTurnos++;
-
-  let seguir = prompt("¿Desea agregar otro tiempo de espera? (S/N)");
-  if (seguir === "n") {
-    seguirAgregando = false;
+// Objeto
+class Article {
+  constructor(nombre, precio, condition) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.condition = condition;
   }
 }
 
-let tiempoPromedio = tiempoTotal / cantidadDeTurnos;
-alert(`El tiempo de espera promedio es de ${tiempoPromedio} minutos.`);
+// Array de artículos
+const articulos = [
+  new Articulos("Camiseta", 1000, "Nueva"),
+  new Articulos("Gorra", 500, "Usada"),
+  new Articulos("Bufanda", 250, "Nueva"),
+  new Articulos("Pantalón", 1500, "Usado")
+];
+
+// Funciones de interacción con el usuario
+function AgregarItem() {
+  const nombre = prompt("Ingrese el nombre del artículo:");
+  const precio = parseInt(prompt("Ingrese el precio del artículo:"));
+  const condition = prompt("Ingrese la condición del artículo (Nueva/Usada):");
+
+  const articulo = new Articulo(nombre, precio, condition);
+  AgregarItem(articulo);
+}
+
+function seleccionArticulo() {
+  const name = prompt("Ingrese el nombre del artículo que desea vender:");
+  const articleIndex = itemsForSale.findIndex(article => article.name === name);
+
+  if (articleIndex !== -1) {
+    seleccionItem(articleIndex);
+  } else {
+    alert("El artículo no se encuentra en venta.");
+  }
+}
+
+// Ciclo principal de interacción con el usuario
+while (true) {
+  const choice = parseInt(prompt("¿Qué desea hacer?\n1. Agregar artículo a la venta\n2. Vender artículo\n3. Salir"));
+
+  switch (choice) {
+    case 1:
+        seleccionArticulo();
+      break;
+    case 2:
+      seleccionArticulo();
+      break;
+    case 3:
+      alert("Gracias por usar nuestro sistema de compra y venta de artículos de San Lorenzo.");
+      break;
+    default:
+      alert("Opción inválida. Por favor, seleccione una opción válida.");
+  }
+
+  if (choice === 3) {
+    break;
+  }
+}
